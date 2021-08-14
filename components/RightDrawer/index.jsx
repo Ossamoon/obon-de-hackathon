7a2173809137e1f9df9ae2b818207be9a42cf2bd
chma19063@g.nihon-u.ac.jp <chma19063@g.nihon-u.ac.jp>
2021-08-14 14:14:15 +0900

    add omen

--- components/RightDrawer/index.jsx
+++ components/RightDrawer/index.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 export default ({mask}) => {
   const [manUrl, womanUrl, omeUrl] = [
-    "https://1.bp.blogspot.com/-vTn-xUxYn3o/YEGQFi9UWHI/AAAAAAABdec/J_dKnbLbh3oGY7IF6YA3p8-sV2fBQni-gCNcBGAsYHQ/s726/nigaoe_ryuuzouji_takanobu.png",
+    "https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png",
     "https://1.bp.blogspot.com/-ZOg0qAG4ewU/Xub_uw6q0DI/AAAAAAABZio/MshyuVBpHUgaOKJtL47LmVkCf5Vge6MQQCNcBGAsYHQ/s1600/pose_pien_uruuru_woman.png",
     "https://1.bp.blogspot.com/-Iavt8UbdbHQ/XhwqDLUxgnI/AAAAAAABW68/VeHORd8e7O4PreNArAUE7URQOwOyHEk5wCNcBGAsYHQ/s1600/omen_tengu.png"
   ];
+
   const [radio, setRadio] = useState('man');
   return (
     <nav className="bg-blue-300" style={{ width: "30%" }}>
@@ -28,12 +29,12 @@ export default ({mask}) => {
       </div>
 
       <p>{radio}</p>
-      <img src = {omeUrl}/>
-      {radio === 'man'?
-      <img src = {manUrl}/>
-      :
-      <img src = {womanUrl}/>}
       
+      {radio === 'man'?
+      <img src = {manUrl} class = "relative top-11 right-2.5" />
+      :
+      <img src = {womanUrl} class = "relative top-11 right-2.5"/>}
+      <img src = {omeUrl} class = "w-8/12 relative bottom-12 left-5" />
     </nav>
   );
 }
