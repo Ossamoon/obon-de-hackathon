99598ba98e6f774ec3977613664151ea047a63aa
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-08-14 22:42:20 +0900

    fix: split OmenModal

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -23,6 +23,53 @@ const OmenCard = ({ src, onClick }: OmenCardProps) => {
   )
 }
 
+interface OmenModalProps {
+  omen: Omen
+  onClose: () => void
+  onPrint: () => void
+}
+
+const OmenModal = ({ omen, onPrint, onClose }: OmenModalProps) => {
+  return (
+    <div className="absolute top-1/2 transform -translate-y-1/2 w-full bg-white z-10">
+      <div className="flex w-full justify-between">
+        <div className="w-6/10 bg-white">
+          <img src={omen.src} alt="" className="w-full" />
+        </div>
+        <div className="flex flex-col justify-between flex-grow bg-gray-200">
+          <div>
+            <span className="ml-2 text-3xl">Test-Title</span><br />
+
+            <span className="ml-2 text-xl">#Tag1</span><br />
+            <span className="ml-2 text-xl">#Tag2</span><br />
+            <span className="ml-2 text-xl">#Tag3</span><br />
+          </div>
+
+          <div className="flex justify-center">
+            <div>
+              <button
+                className="bg-green-200 hover:bg-green-300 m-2 px-12 py-1 text-lg rounded"
+                onClick={onPrint}
+              >
+                Print
+              </button>
+            </div>
+
+            <div>
+              <button
+                className="bg-red-200 hover:bg-red-300 m-2 px-4 py-1 text-lg rounded"
+                onClick={onClose}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 interface Props {
   omens: Omen[]
   isOverlay: boolean
@@ -50,43 +97,16 @@ export const Main = ({ omens, isOverlay, setIsOverlay }: Props) => {
       </div>
 
       {0 <= focusIndex && (
-        <div className="absolute top-1/2 transform -translate-y-1/2 w-full bg-white z-10">
-          <div className="flex w-full justify-between">
-            <div className="w-6/10 bg-white">
-              <img src={omens[focusIndex].src} alt="" className="w-full" />
-            </div>
-            <div className="flex flex-col justify-between flex-grow bg-gray-200">
-              <div>
-                <span className="ml-2 text-3xl">Test-Title-{focusIndex}</span><br />
-
-                <span className="ml-2 text-xl">#Tag1</span><br />
-                <span className="ml-2 text-xl">#Tag2</span><br />
-                <span className="ml-2 text-xl">#Tag3</span><br />
-              </div>
-
-              <div className="flex justify-center">
-                <div>
-                  <button className="bg-green-200 hover:bg-green-300 m-2 px-12 py-1 text-lg rounded"
-                    onClick={() => {
-                      console.log("Print")
-                    }}>
-                    Print
-                  </button>
-                </div>
-
-                <div>
-                  <button className="bg-red-200 hover:bg-red-300 m-2 px-4 py-1 text-lg rounded"
-                    onClick={() => {
-                      setFocusIndex(-1)
-                      setIsOverlay(false)
-                    }}>
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <OmenModal
+          omen={omens[focusIndex]}
+          onPrint={() => {
+            console.log("Print")
+          }}
+          onClose={() => {
+            setFocusIndex(-1)
+            setIsOverlay(false)
+          }}
+        />
       )}
     </nav>
   )
