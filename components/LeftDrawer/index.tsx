74f1212a11566d969bc6d974b84fa1cd7cbe15d2
Ossamoon <osamu950623@gmail.com>
2021-08-14 15:13:12 +0900

    create search view

--- components/LeftDrawer/index.tsx
+++ components/LeftDrawer/index.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export default () => {
+  const [words, setWords] = useState("");
+  const onSubmit = () => {
+    console.log(words);
+  };
+
+  const onChange = (e) => {
+    setWords(e.target.value);
+  };
+
   return (
     <nav className="bg-blue-300" style={{ width: "20%" }}>
-      検索！
+      <div className="w-11/12 h-96 rounded-lg bg-gray-200 mx-auto my-8">
+        <div className="font-bold text-gray-700 p-2">お面を検索</div>
+        <div className="px-2">
+          <input
+            type="text"
+            value={words}
+            onChange={onChange}
+            className="w-full bg-gray-50 rounded-md px-1"
+          />
+          <div
+            className="w-16 text-center bg-blue-400 cursor-pointer my-2"
+            onClick={onSubmit}
+          >
+            検索！
+          </div>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
