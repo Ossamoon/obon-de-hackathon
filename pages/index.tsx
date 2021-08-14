9756cb216f000a8e4da2a157b1d679d06efd2517
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-08-14 17:58:36 +0900

    feat: add overlay

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 import { Omens } from "../assets/omens"
 import LeftDrawer from "../components/LeftDrawer"
 import Main from "../components/Main"
 import RightDrawer from "../components/RightDrawer"
 
 export const Page = () => {
+  const [isOverlay, setIsOverlay] = useState<boolean>(false)
+
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen relative">
       <main className="flex h-full">
         <LeftDrawer />
 
@@ -17,6 +20,10 @@ export const Page = () => {
           omens={[]}
         />
       </main>
+
+      {isOverlay && (
+        <div className="absolute w-full h-full bg-gray-900 opacity-50 top-0 left-0"/>
+      )}
     </div>
   )
 }
