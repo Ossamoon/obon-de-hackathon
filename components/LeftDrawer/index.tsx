9d1cafba69e72e7d9087e2ca893cb627151553e6
Ossamoon <osamu950623@gmail.com>
2021-08-15 11:57:08 +0900

    change left/right drawer width

--- components/LeftDrawer/index.tsx
+++ components/LeftDrawer/index.tsx
@@ -10,8 +10,8 @@ export const LeftDrawer = ({ onSubmit }: LeftDrawerProps) => {
   const [words, setWords] = useState("");
 
   return (
-    <nav className="fixed w-2/10 h-full left-0 top-0 bg-blue-300 ">
-      <img src = "/logo.png" />
+    <nav className="fixed w-3/10 h-full left-0 top-0 bg-blue-300 ">
+      <img src="/logo.png" />
       <div className="w-11/12 rounded-lg bg-gray-200 mx-auto my-8 pb-1">
         <div className="font-bold text-gray-700 p-2">お面を検索</div>
         <div className="px-2">
--- components/RightDrawer/index.tsx
+++ components/RightDrawer/index.tsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
-import { Omen } from "../../interfaces/omen"
+import { Omen } from "../../interfaces/omen";
 
 interface RightDrawerProps {
-  omen: Omen
+  omen: Omen;
 }
 
 export const RightDrawer = ({ omen }: RightDrawerProps) => {
   const urls = {
-    "man": "https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png",
-    "woman": "https://1.bp.blogspot.com/-ZOg0qAG4ewU/Xub_uw6q0DI/AAAAAAABZio/MshyuVBpHUgaOKJtL47LmVkCf5Vge6MQQCNcBGAsYHQ/s1600/pose_pien_uruuru_woman.png",
-    "nogender": ""
-  } as const
+    man: "https://1.bp.blogspot.com/-jlZlCg-8FAM/Xub_u8HTD1I/AAAAAAABZis/ZhUI05AZBEQpVinedZ6Xy-eIucmNuY2SQCNcBGAsYHQ/s1600/pose_pien_uruuru_man.png",
+    woman:
+      "https://1.bp.blogspot.com/-ZOg0qAG4ewU/Xub_uw6q0DI/AAAAAAABZio/MshyuVBpHUgaOKJtL47LmVkCf5Vge6MQQCNcBGAsYHQ/s1600/pose_pien_uruuru_woman.png",
+    nogender: "",
+  } as const;
 
-  const [radio, setRadio] = useState<keyof typeof urls>('man');
+  const [radio, setRadio] = useState<keyof typeof urls>("man");
 
   return (
-    <nav className="fixed w-3/10 h-full right-0 top-0 bg-blue-300">
+    <nav className="fixed w-2/10 h-full right-0 top-0 bg-blue-300">
       <select
         name="gender"
-        onChange={(e) => setRadio(e.target.value as unknown as keyof typeof urls)}
+        onChange={(e) =>
+          setRadio(e.target.value as unknown as keyof typeof urls)
+        }
         className="relative left-2 top-2 block mt-1 rounded border-gray-300 shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
       >
         <option value="man">男性</option>
@@ -29,12 +32,10 @@ export const RightDrawer = ({ omen }: RightDrawerProps) => {
       <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
         <image href={urls[radio]} height="100%" width="100%" />
 
-        {omen && (
-          <image href={omen.src} height="200" width="100%" />
-        )}
+        {omen && <image href={omen.src} height="200" width="100%" />}
       </svg>
     </nav>
   );
-}
+};
 
-export default RightDrawer
+export default RightDrawer;
